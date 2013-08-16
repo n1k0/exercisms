9950a3ce296adaf0230c9edefee470cb08713467
Nicolas Perriault <nperriault@gmail.com>
2013-08-17 00:41:15 +0200

    beer.py version 2

--- python/beer-song/beer.py
+++ python/beer-song/beer.py
@@ -1,13 +1,14 @@
 class Beer(object):
   REFILL = 99
-  TEMPLATE = ("{current} of beer on the wall, {current} of beer.\n" +
+  TEMPLATE = ("{current} of beer on the wall, {repeat_current} of beer.\n" +
               "{action}, {remaining} of beer on the wall.\n")
 
   def verse(self, n):
-    return self._ucfirst(self.TEMPLATE.format(
-      current=self._format_quantity(n),
+    return self.TEMPLATE.format(
+      current=self._format_quantity(n).capitalize(),
+      repeat_current=self._format_quantity(n),
       action=self._format_action(n),
-      remaining=self._format_quantity(self.REFILL if n == 0 else n - 1)))
+      remaining=self._format_quantity(self.REFILL if n == 0 else n - 1))
 
   def sing(self, start, to=0):
     return "\n".join(self.verse(n) for n in range(start, to - 1, -1)) + "\n"
@@ -23,6 +24,3 @@ class Beer(object):
       return "no more bottles";
     return "{number} bottle{plural}".format(number=n,
                                             plural="s" if n > 1 else "");
-
-  def _ucfirst(self, string):
-    return string[0].upper() + string[1:]
