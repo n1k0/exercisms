8c31fb50b86962676a0c380e164d2462a370b8a0
Nicolas Perriault <nperriault@gmail.com>
2013-08-18 23:15:08 +0200

    beer-song elixir version 2

--- elixir/beer-song/beer.exs
+++ elixir/beer-song/beer.exs
@@ -1,24 +1,29 @@
 defmodule Beer do
-  def verse(0) do
-    "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-  end
-  def verse(1) do
-    "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-  end
-  def verse(n) do
-    verse(n, n - 1)
-  end
-  def verse(n, 1) do
-    "#{n} bottles of beer on the wall, #{n} bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-  end
-  def verse(n, remaining) do
-    "#{n} bottles of beer on the wall, #{n} bottles of beer.\nTake one down and pass it around, #{remaining} bottles of beer on the wall.\n"
+  def verse(0),    do: verse(0, 99)
+  def verse(1),    do: verse(1, 0)
+  def verse(n),    do: verse(n, n - 1)
+  def verse(n, r), do: render(n, r)
+
+  def sing(from),     do: sing(from, 0)
+  def sing(from, to), do: assemble(from, to)
+
+  defp format_bottles(0), do: "no more bottles of beer"
+  defp format_bottles(1), do: "1 bottle of beer"
+  defp format_bottles(n), do: "#{n} bottles of beer"
+
+  defp format_action(0), do: "Go to the store and buy some more"
+  defp format_action(1), do: "Take it down and pass it around"
+  defp format_action(n), do: "Take one down and pass it around"
+
+  defp render(n, remaining) do
+    bottles = format_bottles(n)
+    action = format_action(n)
+    remaining = format_bottles(remaining)
+    String.capitalize("#{bottles} on the wall, #{bottles}.\n")
+                   <> "#{action}, #{remaining} on the wall.\n"
   end
 
-  def sing(from) do
-    sing(from, 0)
-  end
-  def sing(from, to) do
+  defp assemble(from, to) do
     Enum.map_join((from..to), "\n", verse &1) <> "\n"
   end
 end
