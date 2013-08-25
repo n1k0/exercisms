4c2f0643e20a34cac73f4e9e38afb2d8628ad22b
Nicolas Perriault <nperriault@gmail.com>
2013-08-25 11:33:55 +0200

    clojure robot-name exercise updated

--- clojure/robot-name/robot.clj
+++ clojure/robot-name/robot.clj
@@ -1,20 +1,7 @@
-(defn- chars-range [from to]
-  (range (int from) (dec (int to))))
+(declare chars-range rand-chars rand-letters
+         rand-digits generate-name)
 
-(defn- rand-chars [n from to]
-  (apply str (->> (chars-range from to)
-                  shuffle
-                  (map char)
-                  (take n))))
-
-(defn- rand-letters [n] (rand-chars n \A \Z))
-
-(defn- rand-digits [n] (rand-chars n \0 \9))
-
-(defn generate-name []
-  (str (rand-letters 2) (rand-digits 3)))
-
-(def robot-defaults {:name ""})
+(def ^:private robot-defaults {:name ""})
 
 (defn robot [] (atom robot-defaults))
 
@@ -25,5 +12,22 @@
                           (fn [_] (generate-name))))
       name)))
 
-(defn- reset-name [robot]
+(defn reset-name [robot]
   (swap! robot (fn [_] robot-defaults)))
+
+;; private members
+
+(defn- chars-range [from to]
+  (range (int from) (dec (int to))))
+
+(defn- rand-chars [n from to]
+  (apply str (->> (chars-range from to)
+                  shuffle
+                  (map char)
+                  (take n))))
+
+(defn- rand-digits [n] (rand-chars n \0 \9))
+(defn- rand-letters [n] (rand-chars n \A \Z))
+
+(defn- generate-name []
+  (str (rand-letters 2) (rand-digits 3)))
