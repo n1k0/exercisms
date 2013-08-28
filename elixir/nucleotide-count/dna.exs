1f392b169466660ea4683f2d16c53c2c1e5398fa
Nicolas Perriault <nperriault@gmail.com>
2013-08-28 15:24:33 +0200

    updated elixir nucleotide-count

--- elixir/nucleotide-count/dna.exs
+++ elixir/nucleotide-count/dna.exs
@@ -1,15 +1,11 @@
 defmodule DNA do
-  @defaults [{?C, 0}, {?G, 0}, {?A, 0}, {?T, 0}]
+  @nucleotides 'CGAT'
 
   def count(sequence, n) do
     Enum.count(sequence, fn(x) -> n == x end)
   end
 
   def nucleotide_counts(sequence) do
-    Enum.reduce(sequence, HashDict.new(@defaults), &update_counts/2)
-  end
-
-  defp update_counts(n, counts) do
-    HashDict.update(counts, n, 1, &1 + 1)
+    HashDict.new @nucleotides, fn n -> {n, count(sequence, n)} end
   end
 end
