3408038c7fa357a651dd84569e0b32b9e7797604
Nicolas Perriault <nicolas@perriault.net>
2017-01-03 23:13:15 +0100

    added Elm phone-number solution

--- elm/phone-number/PhoneNumber.elm
+++ elm/phone-number/PhoneNumber.elm
@@ -1,1 +1,34 @@
 module PhoneNumber exposing (..)
+
+import Char exposing (isDigit)
+
+
+getNumber : String -> Maybe String
+getNumber source =
+    let
+        digits =
+            String.filter isDigit source
+    in
+        if String.length digits < 10 || String.length digits > 11 then
+            Nothing
+        else if String.length digits == 11 then
+            if String.startsWith "1" digits then
+                Just (String.dropLeft 1 digits)
+            else
+                Nothing
+        else
+            Just digits
+
+
+prettyPrint : String -> Maybe String
+prettyPrint source =
+    case getNumber source of
+        Just phone ->
+            ("(" ++ (String.slice 0 3 phone) ++ ") ")
+                ++ (String.slice 3 6 phone)
+                ++ "-"
+                ++ (String.slice 6 10 phone)
+                |> Just
+
+        _ ->
+            Nothing
